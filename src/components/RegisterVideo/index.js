900978e079bdc4fa8530e2a0f483d99a4dada025
LucasCruz <lca.snf21@uea.edu.br>
2022-11-14 00:53:26 -0400

    Create Custom Hook

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,22 +1,56 @@
 import {StyledRegisterVideo} from "./styles"
 import React from "react"
 
+function useForm(propsDoForm){
+    const [values, setValues] = React.useState(propsDoForm.initialValues);
+    return{
+        values,
+        handleChange:(event)=>{
+            const value = event.target.value;
+            const name = event.target.name;
+            setValues({
+                ...values,
+                [name]:value,
+            })
+        },
+        clearForm:()=>{setValues({})}
+    };
+}
+
+
+
+
 export default function RegisterVideo(){
     const [formVisible, setFormVisible] = React.useState(false)
-
+    const formCadastro = useForm({
+        initialValues:{titulo:"black panter", url:"www.google.com"}
+    })
 
     return(
         <StyledRegisterVideo>
             <button className="add-video" onClick={()=>setFormVisible(true)}>
                 +
             </button>
-            {formVisible && (<form>
+            {formVisible && (
+            <form onSubmit={(event)=>{
+                event.preventDefault();
+                setFormVisible(false);
+                formCadastro.clearForm();
+
+            }}>
                 <div>            
-                    <button className="close-modal" onClick={()=>setFormVisible(false)}>
+                    <button type="button" className="close-modal" onClick={()=>setFormVisible(false)}>
                         X
                     </button>
-                    <input placeholder="Título do vídeo" />
-                    <input placeholder="URL" />
+                    <input 
+                    name="titulo" 
+                    value={formCadastro.values.titulo} 
+                    placeholder="Título do vídeo" 
+                    onChange={formCadastro.handleChange} />
+                    <input name="url"
+                    value={formCadastro.values.url} 
+                    placeholder="URL" 
+                    onChange={formCadastro.handleChange}/>
                     <button type="submit">
                         Cadastrar
                     </button>
